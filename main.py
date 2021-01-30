cdbf64466ae578174318e84d490d6d72a69c93bd
notnypical <46350569+notnypical@users.noreply.github.com>
2021-01-30 22:25:26 +0100

    Add basic application

---
+++ main.py
@@ -0,0 +1,35 @@
+# This Python file uses the following encoding: utf-8
+#
+# Copyright 2021 LotrioLabs, <https://lotriolabs.github.io>.
+#
+# This file is part of Lotrio-QtPy.
+#
+# Lotrio-QtPy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Lotrio-QtPy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Lotrio-QtPy.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import sys
+
+from PySide2.QtWidgets import QApplication
+
+from main_window import MainWindow
+
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec_())
